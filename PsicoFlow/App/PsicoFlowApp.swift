ce38df6c945ec2df6ed82f838bff2b5aadde6a2e
kenaygn <kenaystudy@gmail.com>
2026-07-21 23:30:46 -0300

    refactor: funcoes de criação de sessoes e de pagamentos criados no firebase

--- PsicoFlow/App/PsicoFlowApp.swift
+++ PsicoFlow/App/PsicoFlowApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
-import FirebaseAuth // Adicionado para acessar a sessão do usuário logado
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
@@ -19,29 +19,12 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 @main
 struct PsicoFlowApp: App {
-    // TODO: Vai ter que sair pq quem vai carregar as sessoes vai ser um script no Firebase
-    @Environment(\.scenePhase) var scenePhase
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
             RootView()
                 .preferredColorScheme(.light)
-        }
-        
-        // TODO: Remover bloco onChange futuramente quando a automação for para o backend
-        .onChange(of: scenePhase) { novaFase in
-            if novaFase == .active {
-                // Recupera o ID do usuário ativo diretamente do Firebase
-                if let uid = Auth.auth().currentUser?.uid {
-                    
-                    // Se a sua função runStartupChecks foi refatorada para ser 'async',
-                    // basta envolver a linha abaixo em uma Task { await ... }
-                    SystemUpdateManager.shared.runStartupChecks(userId: uid)
-                    
-                }
-            }
         }
     }
 }
